280a445acdc15c26017d65ee947f8813db13b095
Simon Amor <simon@leaky.org>
2019-10-23 08:34:54 +0100

    Show the headers for the request as well as content

--- send.sh
+++ send.sh
@@ -6,6 +6,6 @@ then
     echo "Need a client id"
     exit
 fi
-curl  -k -d "id=$CL" -d 'msg={"text":"Hello World"}' http://localhost:3019/send
+curl -i -k -d "id=$CL" -d 'msg={"text":"Hello World"}' http://localhost:3019/send
 
 echo
